08b29df6ab432222b1c5f4428b9d36ee0f4c4c2a
sam <daniel.jacob.samuel@gmail.com>
2017-09-23 15:57:11 -0700

    raw search integration is hooked up, Dockerfile and dependency management needs to be better so we don't have to rebuild everything all the time

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,10 @@
 FROM tiangolo/uwsgi-nginx-flask:python3.6
 RUN pip install omdb
 COPY omdb.api.key /app/omdb.api.key
+RUN apt-get install -y --force-yes git \
+&& curl -sL https://deb.nodesource.com/setup_6.x | bash - \
+&& apt-get install -y --force-yes nodejs
+RUN npm install -g bower
 COPY ./app /app
+RUN bower --allow-root install bootstrap#v4.0.0-alpha.6
+RUN pip install Flask-WTF
--- app/main.py
+++ app/main.py
@@ -1,13 +1,33 @@
 import flask
 import omdb
 
+from forms import SearchForm
+
+
 app = flask.Flask(__name__)
+app.config.from_object('config')
+
+
 omdb.set_default('apikey', open('omdb.api.key').read().strip())
 
 
 @app.route('/')
 def hello():
-    return flask.jsonify(**dict(omdb.get(title='True Grit', year=1969, fullplot=True, tomatoes=True)))
+    return flask.render_template('starter.html', form=SearchForm(), results='')
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    form = SearchForm()
+    if flask.request.method == 'POST':
+        return flask.redirect((flask.url_for('search_results', search_term=form.search_term.data)))
+    return flask.render_template('starter.html', form=SearchForm(), results='')
+
+
+@app.route('/search_results/<search_term>')
+def search_results(search_term):
+    results = str(omdb.search(search_term))
+    return flask.render_template('starter.html', form=SearchForm(), results=results)
 
 
 if __name__ == '__main__':
